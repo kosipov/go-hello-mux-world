5c6b0bb180afe2e086996d62e8a2dd3014f4903d
kosipov <catalonec1994@gmail.com>
2022-12-26 19:30:57 +0300

    Update main.go

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 	// Server configuration
 	srv := &http.Server{
 		Handler:      r,
-		Addr:         "0.0.0.0:8090",
+		Addr:         "0.0.0.0:8100",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
